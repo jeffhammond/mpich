abdd6c1046c8ecc3b1f0b332b54a0574330d95dc
Jeff Hammond <jeff.science@gmail.com>
2022-11-16 11:21:29 +0200

    transform case-switch to if-else because MPI_Datatype is not required to be a compile-time constant

--- test/mpi/coll/allred.c
+++ test/mpi/coll/allred.c
@@ -267,26 +267,26 @@ static void set_index_power(MPI_Datatype mpi_type, int category, void *arr, int 
     }
 }
 
-#define SET_PAIR_CASE(type, mpi_type) \
-    case mpi_type: \
+#define SET_PAIR_CASE(type, mpi_type_) \
+    if ( mpi_type == mpi_type_ ) \
         { \
             struct type ## _test *pp = p; \
             pp->a = val_a; \
             pp->b = val_b; \
-        }; \
-        break
+        } \
 
 static void set_pair_val(MPI_Datatype mpi_type, void *p, int val_a, int val_b)
 {
-    switch (mpi_type) {
-            SET_PAIR_CASE(int, MPI_2INT);
-            SET_PAIR_CASE(long, MPI_LONG_INT);
-            SET_PAIR_CASE(short, MPI_SHORT_INT);
-            SET_PAIR_CASE(float, MPI_FLOAT_INT);
-            SET_PAIR_CASE(double, MPI_DOUBLE_INT);
-        default:
-            assert(0);
-    }
+        SET_PAIR_CASE(int, MPI_2INT)
+        else
+        SET_PAIR_CASE(long, MPI_LONG_INT)
+        else
+        SET_PAIR_CASE(short, MPI_SHORT_INT)
+        else
+        SET_PAIR_CASE(float, MPI_FLOAT_INT)
+        else
+        SET_PAIR_CASE(double, MPI_DOUBLE_INT)
+        else { assert(0); }
 }
 
 static void set_index_pair_const(MPI_Datatype mpi_type, void *arr, int val_a, int val_b, int count)
@@ -356,8 +356,8 @@ static int cmp_int(void *p, void *q, int type_size)
     }
 }
 
-#define CMP_PAIR_CASE(type, mpi_type) \
-    case mpi_type: \
+#define CMP_PAIR_CASE(type, mpi_type_) \
+    if ( mpi_type == mpi_type_ ) { \
         do { \
             struct type ## _test *pp, *qq; \
             pp = p; \
@@ -367,19 +367,21 @@ static int cmp_int(void *p, void *q, int type_size)
             } else { \
                 return 1; \
             } \
-        } while (0)
+        } while (0); \
+    }
 
 static int cmp_pair(void *p, void *q, MPI_Datatype mpi_type)
 {
-    switch (mpi_type) {
-            CMP_PAIR_CASE(int, MPI_2INT);
-            CMP_PAIR_CASE(long, MPI_LONG_INT);
-            CMP_PAIR_CASE(short, MPI_SHORT_INT);
-            CMP_PAIR_CASE(float, MPI_FLOAT_INT);
-            CMP_PAIR_CASE(double, MPI_DOUBLE_INT);
-        default:
-            assert(0);
-    }
+    CMP_PAIR_CASE(int, MPI_2INT)
+    else
+    CMP_PAIR_CASE(long, MPI_LONG_INT)
+    else
+    CMP_PAIR_CASE(short, MPI_SHORT_INT)
+    else
+    CMP_PAIR_CASE(float, MPI_FLOAT_INT)
+    else
+    CMP_PAIR_CASE(double, MPI_DOUBLE_INT)
+    else { assert(0); }
     return 1;
 }
 
